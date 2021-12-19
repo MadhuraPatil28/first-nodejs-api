6e8e6df538fbfc0eefdbca0d6d4ebb4a3c3890b1
Madhura Patil <patilmadhura28@gmail.com>
2021-12-19 21:31:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,100 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+// Import models
+const Post = require('./src/models/post');
+
+// Define application
+const app = express()
+
+// Define DB Connection
+const db = mongoose.connect('mongodb://localhost:27017/first-node-api-db')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', function(req, res) {
+  // handle the request for root route
+  res.send({ ping: 'pong' })
+})
+
+// Operations: Create, Read, Update, Delete (CRUD)
+app.post('/posts', function(req, res) {
+  // Get values from request payload
+  const title = req.body.title
+  const author = req.body.author
+  const content = req.body.content
+
+  // Assign values to Post model
+  var post = new Post();
+  post.title = title
+  post.author = author
+  post.content = content
+
+  // Save the post
+  post.save(function(error, savedPost) {
+    if(error) {
+      // send error response
+      res.status(500).send({ error: 'Unable to save Post '})
+    } else {
+      // send success response
+      res.status(200).send(savedPost)
+    }
+  })
+});
+
+// 1. Create API to get details of a single Post
+app.get('/posts/:id', function(req, res) {
+  Post.findById(req.params.id, function(error, post) {
+    if(error) {
+      res.status(422).send({error: 'Unable to fetch post!'})
+    }
+    else {
+      res.status(200).send(post)
+    }
+  })
+})
+
+// Get list of all posts
+app.get('/posts', function(req, res) {
+  Post.find({}, function(error, posts) {
+    if(error) {
+      // send error response
+      res.status(422).send({ error: 'Unable to fetch posts '})
+    } else {
+      // send success response
+      res.status(200).send(posts)
+    }
+  })
+})
+
+// 2. Create API to update a Post
+app.put('/posts/:id', function(req, res) {
+  Post.findByIdAndUpdate(req.params.id, req.body, function (error, post){
+    if(error) {
+      res.status(422).send({error: 'Update unsucessful!'})
+    }
+    else {
+      res.status(200).send(post)
+    }
+  })
+})
+
+
+// 3. Create API to delete a Post
+app.delete('/posts/:id', function(req, res) {
+  Post.findByIdAndDelete(req.params.id, function(error, post){
+    if(error){
+      res.status(422).send({error: 'Delete unsuccessful!'})
+    }
+    else{
+      res.status(200).send(post)
+    }
+  })
+})
+
+
+app.listen(3001, function() {
+  console.log('Server is running at port 3001....')
+})
